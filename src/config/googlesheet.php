b8148cb92ad3d90b858d90ac3566765863016bc2
Avik Routh <avik@itobuz.com>
2020-01-17 19:47:09 +0530

    Settings suggetion changed at config >> googlesheet.php

--- src/config/googlesheet.php
+++ src/config/googlesheet.php
@@ -1,17 +1,17 @@
 <?php
 return [
     'config' => [        
-        'client_id'=>'526569989960-tqktuj966nt2eld1mubm0vqatji4ln3q.apps.googleusercontent.com',
-        'project_id'=>'polished-core-259509',
+        'client_id'=>'<Google Client ID>',
+        'project_id'=>'<Google Project ID>',
         'auth_uri'=>'https://accounts.google.com/o/oauth2/auth',
         'token_uri'=>'https://oauth2.googleapis.com/token',
         'auth_provider_x509_cert_url'=>'https://www.googleapis.com/oauth2/v1/certs',
-        'client_secret'=>'UegYPuKV9nkDfM4H-vpe-K35',
-        'redirect_uris'=>["http://localhost:8000/userAuthenticated"],
-        'javascript_origins'=>["http://localhost:8000"]
+        'client_secret'=>'<Google Client secret>',
+        'redirect_uris'=>[env('APP_URL')."/userAuthenticated"],
+        'javascript_origins'=>[env('APP_URL')]
     ],
 
-    'project_number'=>'526569989960',
+    'project_number'=>'<Google Project number>',
 
     'return_url' => env('GOOGLE_SHEET_RETURN_URL', "#"),
     'auto_auth' => false
